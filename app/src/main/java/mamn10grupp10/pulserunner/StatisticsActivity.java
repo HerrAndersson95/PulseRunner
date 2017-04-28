a195b87ddeab2786f2da63fcc7d9ede1b8ebbc1a
JakeTheMFSnake <jakob.hakansson@outlook.com>
2017-04-28 15:40:22 +0200

    Testing of my methods for read/write
    Det vi behöver göra nu är att få dem att sparas till en fil med

--- app/src/main/java/mamn10grupp10/pulserunner/StatisticsActivity.java
+++ app/src/main/java/mamn10grupp10/pulserunner/StatisticsActivity.java
@@ -7,25 +7,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class StatisticsActivity extends AppCompatActivity {
     private Vibrator vib;
     private int speed;
     private int sleep;
     private TextView tw;
     private TextView twSleep;
+
+    //Test
+    private TextView twLoader;
+    private ArrayList<Double> listOfValues = new ArrayList<Double>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        listOfValues.add(50.0);
+        listOfValues.add(30.0);
+        listOfValues.add(40.0);
+        listOfValues.add(50.0);
+        listOfValues.add(60.0);
+
         speed = 100;
         sleep = 100;
         vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         tw = (TextView) findViewById(R.id.infoBar);
         twSleep = (TextView) findViewById(R.id.sleepTime);
+        twLoader = (TextView) findViewById(R.id.loadedText);
 
         tw.setText("Speed: "+speed+" ms");
         twSleep.setText("Sleep: "+sleep+" ms");
+        twLoader.setText("Loader...");
     }
 
     public void onClickClose(View v){
@@ -85,6 +99,39 @@ public class StatisticsActivity extends AppCompatActivity {
         tw.setText("Speed: "+speed+" ms");
         long[] pattern = {0,speed,sleep};
         vib.vibrate(pattern,0);
+    }
+
+
+
+    private String trackName = "Träskrundan";
+    private String runnerName = "Christer Sjögren";
+    private String savedData;
+
+    public void onClickLoad(View v){
+        twLoader.setText("Load Button Pressed");
+        ReadFromFile rfw = new ReadFromFile();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Get TrackName: "+rfw.getTrackName(savedData)+"\n");
+        sb.append("Get RunnerName: "+rfw.getRunnerName(savedData)+"\n");
+        ArrayList<Double> list = rfw.returnDiffArray(savedData);
+        for(double value: list){
+            sb.append(value+"\n");
+        }
+        twLoader.setText(sb.toString());
+
+
+
+    }
+
+    public void onClickSave(View v){
+        twLoader.setText("Save Button pressed.");
+        WriteToFile wf = new WriteToFile();
+        String data = wf.createStringFile(trackName,runnerName,listOfValues);
+        //savedData = savedData +"\n"+"\n" + data;
+        savedData = data;
+        twLoader.setText("Save Button pressed \n"+data);
+        /*
+        wf.saveFile(wf.createStringFile(trackName,runnerName,listOfValues));*/
     }
 
 
