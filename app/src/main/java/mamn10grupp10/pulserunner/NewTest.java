2429873f691e5b0991fe4de9396383851f236eb1
Anne Line Gjersem <anne_line94@hotmail.com>
2017-05-03 19:58:40 +0200

    testupdate

--- app/src/main/java/mamn10grupp10/pulserunner/NewTest.java
+++ app/src/main/java/mamn10grupp10/pulserunner/NewTest.java
@@ -13,27 +13,45 @@ import android.view.View;
 
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.String;
 
 
-   public class NewTest extends AppCompatActivity{
+   public class NewTest extends AppCompatActivity {
 
-    TextView txtv;
-    EditText txt;
-   Button save;
-    Button load;
-
-
-public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_new_test);
+       TextView textv;
+       EditText txt;
+       Button save;
+       Button load;
 
 
+       public void onCreate(Bundle savedInstanceState) {
+           super.onCreate(savedInstanceState);
+           setContentView(R.layout.activity_new_test);
+           txt = (EditText) findViewById(R.id.write);
+           textv = (TextView) findViewById(R.id.read);
+           save = (Button) findViewById(R.id.save);
+           load = (Button) findViewById(R.id.load);
 
-}
 
-public void writeFile(View view ){
+       }
 
-}
+       public void writeFile(View view) {
 
-}
+       }
+
+       public void readFile(View view) {
+           String txt;
+           try {
+               InputStream is = getAssets().open("distance.txt");
+               byte[] buffer = new byte[is.available()];
+               is.read();
+               is.close();
+               txt = new String(buffer);
+                    textv.setText(txt);
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
+   }
