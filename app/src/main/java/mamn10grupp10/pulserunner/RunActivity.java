f1319bb45c7d02fd07c3efe7f096e3250251ca53
Annie Önnered <annie.onnered@gmail.com>
2017-05-08 17:38:39 +0200

    Filename from RunActivity view gives value to next RunActivityRunning
    Filename from RunActivity view gives value to next RunActivityRunning.
    Ännu inte i array!

--- app/src/main/java/mamn10grupp10/pulserunner/RunActivity.java
+++ app/src/main/java/mamn10grupp10/pulserunner/RunActivity.java
@@ -1,18 +1,27 @@
 package mamn10grupp10.pulserunner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+
 public class RunActivity extends AppCompatActivity {
     ListView list;
+    TextView view;
+    ListAdapter adapter;
+    String[] tracks;
+    String selectedTrack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +29,28 @@ public class RunActivity extends AppCompatActivity {
         setContentView(R.layout.activity_run);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         list =(ListView)findViewById(R.id.listview);
-        String[] tracks = getFilesDir().list();
-        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tracks);
+        view = (TextView)findViewById(R.id.textView11);
+        tracks = getFilesDir().list();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tracks);
         list.setAdapter(adapter);
+
+
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectedTrack = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(RunActivity.this, selectedTrack, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
     }
+
 
     public void onClickStart(View v){
         Intent intent = new Intent(this, RunActivityRunning.class);
+        intent.putExtra("selectedTrack",selectedTrack);
         startActivity(intent);
     }
 
